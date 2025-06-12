94bfd22adb2c489d2486a6a115a5b41930f3c962
Aditya Shukla <aditya.shukla.professional@gmail.com>
2025-06-12 09:12:58 +0000

    Initial Commit

---
+++ src/Components/Chess/Chess.jsx
@@ -0,0 +1,82 @@
+import React,{useState} from 'react'
+import './Chess.css'
+import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
+
+const ChessGame = () => {
+  const [game,setGame]=useState(new Chess());
+  const [fen,setFen]=useState("start");
+  const [moveSquares, setMoveSquares] = useState({});
+  const [winner,setWinner]=useState("");
+
+  function onDrop(sourceSquare,targetSquare){
+    try{
+        if (game.isGameOver()) return false;
+        const move=game.move({
+            from:sourceSquare,
+            to:targetSquare,
+            promotion:"q"
+        })
+
+        if(move===null) return false;
+
+        setFen(game.fen());
+
+        if(game.isGameOver()){
+          const lastTurn=game.turn()==="w"?"Black":"White";
+          setWinner(`${lastTurn} wins! 🥳`)
+        }
+        return true
+    }catch(error){
+        console.error("Invalid Move!",error)
+        return false;
+    }
+  }
+  function resetGame() {
+    const newGame = new Chess();
+    setGame(newGame);
+    setFen("start");
+    setWinner("");
+    setMoveSquares({});
+  }
+  function onMouseOverSquare(square){
+    const piece=game.get(square);
+    if (!piece || piece.color!==game.turn()) return;
+
+    const moves=game.moves({square,verbose:true});
+    if(!moves.length){
+        setMoveSquares({});
+        return;
+    }
+    const newSquares={};
+    newSquares[square] = {
+      boxShadow: "inset 0 0 12px rgba(255,255,255,0.8)"
+    };
+    moves.forEach(move=>{
+        newSquares[move.to]={
+            background:move.captured ? "radial-gradient(circle, red 30%, transparent 40%)" : "radial-gradient(circle, green 30%, transparent 40%)",
+            borderRadius:"50%"
+        }
+    })
+
+    setMoveSquares(newSquares)
+  }
+
+  function onMouseOutSquare(){
+    setMoveSquares({});
+  }
+  
+  return (
+    <div>
+        <h1 className="text">{winner ? winner : "Let's Play a Round of Chess 🧐"}</h1>
+        <div className="outerContainer">
+            <div className="chessContainer">
+              <Chessboard position={fen} boardWidth={Math.min(window.innerWidth - 60, 500)} onMouseOutSquare={onMouseOutSquare} onMouseOverSquare={onMouseOverSquare} customSquareStyles={moveSquares} onPieceDrop={onDrop}/>
+            </div>
+        </div>
+        <button className="restartButton" onClick={resetGame}>Restart Game</button>
+    </div>
+  )
+}
+
+export default ChessGame
